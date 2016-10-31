407cb9b61c29131b346d90b48ef2f5c609d3a469
startxfr <clarue@startx.fr>
2016-10-31 03:03:06 +0100

    many change in cli for adding project, service (form sxapi-sample) build, start, stop, log. Auto-download sxapi-sample if not present

--- cli.sh
+++ cli.sh
@@ -1,87 +1,176 @@
 #!/bin/bash
+CWD=$(pwd)
 
-function displayStartCli {
-    echo "" 
-    echo "==================================" 
-    echo "== SXAPI CLI (dev)"
-    echo "==================================" 
-    echo ""
+CLI_VERSION="dev"
+CLI_PATH=~/.sxapi-cli
+CLI_PATH_SAMPLE=$CLI_PATH/sample/samples
+
+SAMPLE_PROJECT="sxapi-sample"
+SAMPLE_REPO_PROJECT="startxfr/$SAMPLE_PROJECT"
+SAMPLE_REPO_VERSION="dev"
+
+#GITHUB_API=https://api.github.com/repos/$SAMPLE_REPO_PROJECT/tags
+
+
+function displayUsage {
+    cat <<EOF
+Usage:
+  sxapi-cli COMMAND [ARGS...]
+  sxapi-cli COMMAND -h|--help
+
+Options:
+  -v, --version      Print version and exit
+
+Commands:
+  project            project informations
+  setup              setup service
+  build              build service
+  start              start service
+  log                log service
+  stop               stop service
+EOF
+exit
 }
-function displayMenu { 
-    echo "1. New microservice"
-    echo "2. Use microservice"
-    echo -n "Enter your choice : "
-    read menu
-    if  [  "$menu" == "1"  ]; then
-        displayNewMicroservice
-    elif  [  "$menu" == "2"  ]; then
-        displayNewMicroservice
-        exit 1
-    else
-        displayMenu
-    fi
-}
 
-function displayNewMicroservice { 
-        echo "--- New microservice"
-    echo -n "service name : "
-    read name
-}
-
-
-function checkRootAccess {
-    if [ "$(id -u)" != "0" ]; then
-        echo "! This script must be run as root"
-        echo "! Please run 'su root' and start again"
-        echo "! this installer"
+function displayProject { 
+    if [ ! -d $CWD ]; then
+        echo "$CWD is not a directory"
         exit;
-    else 
-        echo " - root access : OK"
     fi
+    if ! git rev-parse --git-dir > /dev/null 2>&1; then
+        echo "current directory is not versionned"
+        exit;
+    fi
+    REMOTE_PROJECT_REPO=$(git config --get remote.origin.url)
+    echo " - project linked to" $REMOTE_PROJECT_REPO
+    exit;
+}
+
+function displaySetup {
+    arr=(`echo ${params}`);
+    MODEL="${arr[1]}"
+    if [[ "$MODEL" == "" ]]; then
+        MODEL="simple/txt"
+    fi
+    if [ ! -d $CLI_PATH_SAMPLE/$MODEL ]; then
+        echo " - model $MODEL NOT FOUND"
+        echo "   choose one of these"
+        find $CLI_PATH_SAMPLE -maxdepth 2 -mindepth 2
+        exit;
+    fi
+    
+    if [ "$(ls -A $CWD)" ]; then
+        echo " - directory is not empty"
+        echo -n "   overwrite files [y/N] : "
+        read choice
+        if  [[  "$choice" == 'y' || "$choice" == 'Y'  ]]; then
+            cp -rf $CLI_PATH_SAMPLE/$MODEL/* $CWD/
+            cp -rf $CLI_PATH_SAMPLE/$MODEL/.*.yml $CWD/ &> /dev/null
+        else 
+            exit;
+        fi
+    else
+        cp -rf $CLI_PATH_SAMPLE/$MODEL/* $CWD/
+        cp -rf $CLI_PATH_SAMPLE/$MODEL/.*.yml $CWD/ &> /dev/null
+    fi
+    echo " - project setup with $MODEL"
+    exit; 
+}
+
+function displayBuild {
+    docker-compose build 
+}
+
+function displayStart {
+    docker-compose up -d 
+}
+
+function displayLogs {
+    docker-compose logs
+}
+
+function displayStop {
+    docker-compose kill
+    docker-compose rm -f
 }
 
 function checkEnviromentConfig {
-    CLI_PATH=/var/sxapi-cli
-    CLI_PATH_SAMPLE=$CLI_PATH/sample
     if [ ! -d $CLI_PATH ]; then
         echo " - $CLI_PATH : NOT FOUND"
         echo -n "   Creating $CLI_PATH environement "
         mkdir -p $CLI_PATH
         echo "DONE"
-    else 
-        echo " - $CLI_PATH : FOUND"
     fi
     if [ ! -d $CLI_PATH_SAMPLE ]; then
         echo " - $CLI_PATH_SAMPLE : NOT FOUND"
         echo -n "   Installing $CLI_PATH_SAMPLE "
         mkdir -p $CLI_PATH_SAMPLE
         installSample $CLI_PATH_SAMPLE
-        echo "DONE"
-    else 
-        echo " - $CLI_PATH_SAMPLE : FOUND"
     fi
 }
 
 function installSample {
     DEST_DIR=${1}
-    VERSION="dev"
-    URL=https://github.com/startxfr/sxapi-sample/archive/$VERSION.zip
+    URL=https://github.com/$SAMPLE_REPO_PROJECT/archive/$SAMPLE_REPO_VERSION.zip
     if curl --output /dev/null --silent --head --fail "$URL"; then
-            curl --silent -L "$URL" > $DEST_DIR/$VERSION.zip
-            cd $DEST_DIR/
-            unzip $VERSION.zip &> /dev/null
-            mv sxapi-sample-dev/* .
-            rm -rf $VERSION.zip
-            rm -rf sxapi-sample-$VERSION
-            echo "DONE"
-        else
-            echo "ERROR"
-            echo "   Could not download sxapi-sample v$VERSION" 
-            exit;
-        fi
+        curl --silent -L "$URL" > $DEST_DIR/$SAMPLE_REPO_VERSION.zip
+        cd $DEST_DIR/
+        unzip $SAMPLE_REPO_VERSION.zip &> /dev/null
+        mv $SAMPLE_PROJECT-$SAMPLE_REPO_VERSION/* .
+        rm -rf $SAMPLE_REPO_VERSION.zip
+        rm -rf $SAMPLE_PROJECT-$SAMPLE_REPO_VERSION
+        echo "DONE"
+    else
+        echo "ERROR"
+        echo "   Could not download $SAMPLE_PROJECT v$SAMPLE_REPO_VERSION" 
+        exit;
+    fi
 }
 
-displayStartCli
-checkRootAccess
+function displayVersion {
+    LONG="0"
+    for i in $params
+    do
+        case $i in
+            -l|--long)
+            LONG="1"
+            ;;
+        esac
+    done
+    
+    if  [ "$LONG" -eq "1" ]; then
+    echo "sxapi-cli : v$CLI_VERSION"
+    else
+    echo $CLI_VERSION
+    fi
+}
+
 checkEnviromentConfig
-displayMenu
+key="$1"
+params="$@"
+case "$key" in
+    -v|version)
+    displayVersion
+    ;;
+    -p|project)
+    displayProject
+    ;;
+    -s|setup)
+    displaySetup
+    ;;
+    build)
+    displayBuild
+    ;;
+    start)
+    displayStart
+    ;;
+    log)
+    displayLogs
+    ;;
+    stop)
+    displayStop
+    ;;
+    *)
+    displayUsage
+    ;;
+esac
